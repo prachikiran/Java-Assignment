87d1c3453ef499df24b76a4d52253d56f92a15c0
prachikiran <prachi.kiran@prorigosoftware.com>
2022-01-14 17:32:49 +0530

    removing the duplicate files

---
+++ ReverseString.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a string to check: ");
+		String input = sc.next();
+		reverseFunction(input);
+		sc.close();
+	}
+
+	public static void reverseFunction(String stringValue) {
+
+		String reverse = "";
+		int length = stringValue.length();
+
+		for (int i = length - 1; i >= 0; i--) {
+			reverse = reverse + stringValue.charAt(i);
+		}
+		System.out.println("The reversed string is: " + reverse);
+	}
+}
