4d82a7e3c99cefccd8aa93a66896499120e0e91b
prachikiran <prachi.kiran@prorigosoftware.com>
2022-01-27 19:02:19 +0530

    File is modified

--- EvenNumbersUsingStream.java
+++ EvenNumbersUsingStream.java
@@ -7,6 +7,8 @@ public class EvenNumbersUsingStream {
 		
 		List<Integer> list = Arrays.asList(19,90,15,84,25,45,28,95,10,12);
 		
+		System.out.println("Even numbers from the list are: ");
+		
 		list.stream()
 			.filter(integer -> integer % 2 == 0)
 			.forEach(System.out::println);
