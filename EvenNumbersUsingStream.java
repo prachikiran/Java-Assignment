9275add33b71d8a29c3717816e5631930d03d08f
prachikiran <prachi.kiran@prorigosoftware.com>
2022-01-27 18:41:06 +0530

    New file added

---
+++ EvenNumbersUsingStream.java
@@ -0,0 +1,14 @@
+import java.util.List;
+import java.util.Arrays;
+
+public class EvenNumbersUsingStream {
+	
+	public static void main(String[] args) {
+		
+		List<Integer> list = Arrays.asList(19,90,15,84,25,45,28,95,10,12);
+		
+		list.stream()
+			.filter(integer -> integer % 2 == 0)
+			.forEach(System.out::println);
+	}
+}
